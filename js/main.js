f4baebeb816c86b95da35a422403b76a7ac32641
Renan Deswarte <renandeswarte@gmail.com>
2015-09-20 23:31:47 -0700

    Functional snake game, random food added & growing snake

--- js/main.js
+++ js/main.js
@@ -2,180 +2,202 @@ $(function(){
 
   var snakePosition_left;
   var snakePosition_top;
-  var belly = {};
+  var snakeBody = [];
+  var snakeBodyLength = 15;
+  var extramove;
+
+  // Add roundTo method for multiple of 20
+  Number.prototype.roundTo = function(num) {
+    var resto = this%num;
+    if (resto <= (num/2)) { 
+        return this-resto;
+    } else {
+        return this+num-resto;
+    }
+  }
 
   initiate();
+  bindEvents();
+  var food = showFood();
 
-// Initiate Zero Data
+  // Initiate snake position
   function initiate(){
 
     snakePosition_left = 0;
     snakePosition_top = 0 ;
 
-    $('.snake').css({
-        "top":snakePosition_top + "px",
-        "left": snakePosition_left +"px"
+    if (snakeBodyLength > 15) {
+      for (var i = 16; i <= snakeBodyLength; i++) {
+        $('.snakeBody-' + i).remove();
+      }
+    }
+    snakeBodyLength = 15;
+
+    $('.snakeHead').css({
+      "top":snakePosition_top + "px",
+      "left": snakePosition_left +"px"
     });
 
-    for (var i = 1; i <= 15 ; i++) {
-        belly[i] = {};
-        belly[i]["top"] = 0;
-        belly[i]["left"] = 0;
-        $('.belly-'+i).css({
-            "top":0 + "px",
-            "left":0 + "px",
-        });
-    };
-}
+    for (var i = 1; i <= snakeBodyLength ; i++) {
+      snakeBody[i] = {};
+      snakeBody[i].top = 0;
+      snakeBody[i].left = 0;
+      $('.snakeBody-'+i).css({
+        "top":0 + "px",
+        "left":0 + "px",
+      });
+    }
+  }
 
-
-var extramove;
-
-// Movement Continous
-function movement(direction,move){
+  // Define the snake continuous movement and check impacts
+  function movement(direction,move){
     window.clearInterval(extramove);
 
-    belly[1]["left"] = snakePosition_left;
-    belly[1]["top"] = snakePosition_top;
+    snakeBody[1].left = snakePosition_left;
+    snakeBody[1].top = snakePosition_top;
 
-    if (direction == "left") {
+    if (direction === "left") {
 
-        snakePosition_left  = snakePosition_left + move;
+      snakePosition_left  = snakePosition_left + move;
+      $('.snakeHead').css(direction,snakePosition_left + "px");
+      eatFood();
+      extramove = setInterval(function() { movement(direction,move); }, 300);
 
-        // Test Impacts
-        if ( bodyImpact(snakePosition_left,snakePosition_top,belly) == false ){
-            return;
-        };
-        if ( borderImpact(snakePosition_left) == false ){
-            return;
-        };
+       // Test Impacts
+      bodyImpact(snakePosition_left,snakePosition_top,snakeBody);
+      borderImpact(snakePosition_left);
 
-        $('.snake').css(direction,snakePosition_left + "px");
-        extramove = setInterval(function () {movement(direction,move)}, 300);
+    } else if (direction === "top") {
 
-    } else if (direction == "top") {
-        snakePosition_top  = snakePosition_top + move;
+      snakePosition_top  = snakePosition_top + move;
+      $('.snakeHead').css(direction,snakePosition_top + "px");
+      eatFood();
+      extramove = setInterval(function() { movement(direction,move); }, 300);
 
-        // Test Impacts
-        if ( bodyImpact(snakePosition_left,snakePosition_top,belly) == false ){
-            return;
-        };
-        if ( borderImpact(snakePosition_top) == false ){
-            return;
-        };
+      // Test Impacts
+      bodyImpact(snakePosition_left,snakePosition_top,snakeBody);
+      borderImpact(snakePosition_top);
+    }
 
-        $('.snake').css(direction,snakePosition_top + "px");
-        extramove = setInterval(function () {movement(direction,move)}, 300);
-    };
-
-
-    $('.belly-1').css({
-        "top":belly[1]["top"] + "px",
-        "left":belly[1]["left"] + "px"
+    $('.snakeBody-1').css({
+      "top":snakeBody[1].top + "px",
+      "left":snakeBody[1].left + "px"
     });
 
-    for (var i = 15; i > 1; i--) {
-        belly[i]["top"] = belly[i-1]["top"];
-        belly[i]["left"] = belly[i-1]["left"];
-        $('.belly-'+i).css({
-            "top":belly[i]["top"] + "px",
-            "left":belly[i]["left"] + "px",
-            "z-index": 15 - (i-1)
+    for (var i = snakeBodyLength; i > 1; i--) {
+      snakeBody[i].top = snakeBody[i-1].top;
+      snakeBody[i].left = snakeBody[i-1].left;
+      $('.snakeBody-'+i).css({
+        "top":snakeBody[i].top + "px",
+        "left":snakeBody[i].left + "px",
+        "z-index": snakeBodyLength - (i - 1)
+      });
+
+    }
+  }
+
+  // Check Border Impact
+  function borderImpact(position){
+    if (position === 600 || position < 0 ) {
+      impactAlert();
+    }
+  }
+
+  //Check Snake Belly impact
+  function bodyImpact(head_left,head_top,body){
+    for (var i = 1; i <= snakeBodyLength ; i++) {
+      if ( head_top === snakeBody[i].top && head_left === snakeBody[i].left) {
+        impactAlert();
+      }
+    }
+  }
+
+  // Display Impact Message & Reset
+  function impactAlert() {
+    $("#impact-popup").fadeIn('fast');
+
+    // Reinitiate the snake position and movement
+    initiate();
+    window.clearInterval(extramove);
+  }
+
+  function bindEvents() {
+    // Remove impact popup on Return / Click  
+    $(document).on('keydown', function(e){
+      if ( e.keyCode === 13 || e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 40) {
+        $("#impact-popup").fadeOut('fast');
+      }
+    });
+    $(document).on("click", function() {
+      $("#impact-popup").fadeOut('fast');
+    });
+  }
+   
+  // Action when a direction key is pressed, Snake movement
+  function keyAction(limit, direction, posCheck, movementLength) {
+    if (direction >= limit) {
+      movement(posCheck,movementLength);
+      bodyImpact(snakePosition_left,snakePosition_top,snakeBody);
+    } else {
+      impactAlert();
+    }
+  }
+
+  // Snake keyboard move
+  $(document).keydown(function(e) {
+    if (e.which === 37) {
+      keyAction(20, snakePosition_left, "left",-20);
+    } else if (e.which === 38) {
+      keyAction(20, snakePosition_top, "top",-20);
+    } else if (e.which === 39) {
+      keyAction(snakePosition_left, 560, "left",20);
+    } else if (e.which === 40) {
+      keyAction(snakePosition_top, 560, "top",20);
+    }
+  });
+
+  // Add food to the game
+  function addFood() {
+    var newFood = {
+      top: rdmNumber(),
+      left: rdmNumber()
+    }
+    for (var i = 1; i <= snakeBodyLength ; i++) {
+      if ( newFood.top === snakeBody[i].top && newFood.left === snakeBody[i].left) {
+        addFood();
+      }
+    }
+    return newFood;
+  }
+
+  // Show Food
+  function showFood() {
+    var food = addFood();
+    $(".main").append('<span class="food" style="left: ' + food.left + 'px;top: ' + food.top + 'px"></span>');
+    return food;
+  }
+
+  // Create a random number between 0 and 580, multiple of 20
+  function rdmNumber() {
+    var number = Math.floor((Math.random() * 580));
+    return number.roundTo(20);
+  }
+
+  // Action when the snake eat the food
+  function eatFood() {
+    if ( food.top === snakePosition_top && food.left === snakePosition_left) {
+        $(".food").remove();
+        snakeBodyLength++;
+        $(".main").append('<div class="snakeBody snakeBody-' + snakeBodyLength + '"></div>');
+        snakeBody[snakeBodyLength] = {};
+        snakeBody[snakeBodyLength].top = snakeBody[snakeBodyLength - 1].top;
+        snakeBody[snakeBodyLength].left = snakeBody[snakeBodyLength - 1].left;
+        $('.snakeBody-'+snakeBodyLength).css({
+          "top":snakeBody[snakeBodyLength - 1].top + "px",
+          "left":snakeBody[snakeBodyLength - 1].left + "px",
         });
-
-    };
-}
-
-// Check Border Impact
-function borderImpact(direction){
-    if (direction == 600 || direction < 0 ) {
-        alert('Game Over');
-        console.log('Border IMPACT !!!!');
-        initiate();
-        return false;
-    }
-}
-
-//Check Snake Belly impact
-function bodyImpact(head_left,head_top,body){
-for (var i = 1; i <= 15 ; i++) {
-     if ( head_top == belly[i]["top"] && head_left == belly[i]["left"]) {
-        alert('Game Over');
-        console.log('Body IMPACT !!!!');
-        initiate();
-        return false;
-     };
-    };
-}
-
-
-
-// Snake keyboard move
-$(document).keydown(function(e) {
-    switch(e.which) {
-
-        //Left Arrow
-        case 37: 
-        if (snakePosition_left >= 20) {
-            movement("left",-20);
-            bodyImpact(snakePosition_left,snakePosition_top,belly);
-        }else{
-            alert('Game Over');
-            console.log('Border IMPACT !!!!');
-            initiate();
-            window.clearInterval(extramove);
-            return;
-        }
-        break;
-
-        //Up Arrow
-        case 38: 
-        if (snakePosition_top >= 20) {
-            movement("top",-20);
-            bodyImpact(snakePosition_left,snakePosition_top,belly);
-
-        }else{
-            alert('Game Over');
-            console.log('Border IMPACT !!!!');
-            initiate();
-            window.clearInterval(extramove);
-            return;
-        };
-        break;
-
-        //Right Arrow
-        case 39: 
-        if (snakePosition_left <= 560) {
-            movement("left",20);
-            bodyImpact(snakePosition_left,snakePosition_top,belly);
-        }else{
-            alert('Game Over');
-            console.log('Border IMPACT !!!!');
-            initiate();
-            window.clearInterval(extramove);
-            return;
-        };
-        break;
-
-        //Down Arrow
-        case 40: 
-        if (snakePosition_top <= 560) {
-            movement("top",20);
-            bodyImpact(snakePosition_left,snakePosition_top,belly);
-        }else{
-            alert('Game Over');
-            console.log('Border IMPACT !!!!');
-            initiate();
-            window.clearInterval(extramove);
-            return;
-        };
-        break;
-
-        default: return;
-    }
-    e.preventDefault();
-});
-
-
+        food = showFood();
+      }
+  }
 
 });
